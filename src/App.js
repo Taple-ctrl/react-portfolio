a9d8da0ce30b89718b1ecc53e9ec75b68d4e3ae0
Pavel <70695203+Taple-ctrl@users.noreply.github.com>
2023-02-13 21:16:19 +0700

    add react nav
    import react-router-dom
    change <a> to <Navlink>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./assets/main.css"
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
@@ -12,14 +12,16 @@ import Project from "./pages/Project";
 function App() {
   return (
     <div className="App">
-
-      <Navbar />
-      {/* <Home /> */}
-      {/* <Projects /> */}
-      <Project />
-      {/* <Contacts /> */}
-      <Footer />
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}  />
+          <Route path="/projects" element={<Projects />}  />
+          <Route path="/project" element={<Project />}  />
+          <Route path="/contacts" element={<Contacts />}  />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,20 +3,41 @@ import "./style.css"
 import sun from "./../../assets/img/icons/sun.svg"
 import moon from "./../../assets/img/icons/moon.svg"
 
+import { NavLink } from "react-router-dom"
+
 const Navbar = () => {
+
+    const activeLink = "nav-list__link nav-list__link--active"
+    const normalLink = "nav-list__link"
+
+
     return (
         <nav className="nav">
             <div className="container">
                 <div className="nav-row">
-                    <a href="./index.html" className="logo"><strong>Freelancer</strong> portfolio</a>
+                    <NavLink to="/" className="logo">
+                        <strong>Freelancer</strong> portfolio
+                    </NavLink>
                     <button className="dark-mode-btn">
                         <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
                         <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
                     </button>
                     <ul className="nav-list">
-                        <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                        <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                        <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
+                        <li className="nav-list__item">
+                            <NavLink to="/" className={ ({isActive})=> isActive ? activeLink : normalLink }>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-list__item">
+                            <NavLink to="/projects" className={ ({isActive})=> isActive ? activeLink : normalLink }>
+                                Projects
+                            </NavLink>
+                        </li>
+                        <li className="nav-list__item">
+                            <NavLink to="/contacts" className={ ({isActive})=> isActive ? activeLink : normalLink }>
+                                Contacts
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
